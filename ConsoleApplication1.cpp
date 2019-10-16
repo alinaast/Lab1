f48b1942a92023c758242e0b51e896813e411af9
alinaast <55184585+alinaast@users.noreply.github.com>
2019-10-16 11:39:46 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿
+
+#include <iostream>
+using namespace std;
+/*int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << endl;
+	cout << 2 * (a + b) << endl;
+	return 0;
+} */
+
+/*int main() {
+	int d;
+	float pi = 3.14;
+	cin >> d;
+	cout << d * pi;
+
+	return 0;
+} */
+
+/*int main() {
+	float a, b;
+	cin >> a >> b;
+	cout << (a + b) / 2;
+	return 0;
+} */
+
+/* int main() {
+	int a, b;
+	cin >> a >> b;
+	a = pow(a, 2);
+	b = pow(b, 2);
+	int s = a + b;
+	int r = a - b;
+	int p = a * b;
+	int h = a / b;
+	cout << s << " " << r << " " << p << " " << h;
+	 return 0;
+} */
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	a = abs(a);
+	b = abs(b);
+	int s = a + b;
+	int r = a - b;
+	int p = a * b;
+	int h = a / b;
+	cout << s << " " << r << " " << p << " " << h;
+	return 0;
+} 
+
+
